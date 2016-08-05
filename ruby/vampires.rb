b3da9612b3e672cdc5e80247aa485a4c35ec8d0a
Collin Rea <collinr01@gmail.com>
2016-08-05 13:24:49 -0500

    Added additional while loop to test for allergies for release 4

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,51 +1,76 @@
 
 def vamp
-  puts "What is your name?"
-  name = gets.chomp
-  name.downcase!
 
-  puts "How old are you?"
-  age = gets.chomp.to_i
+  puts "How many employees will be processed?"
+  employees = gets.chomp.to_i
 
-  puts "What year were you born?"
-  born = gets.chomp.to_i
+  while employees > 0   
+    puts "What is your name?"
+    name = gets.chomp
+    name.downcase!
 
-  puts "Our company cafeteria serves garlic bread. Should we order some for you?"
-  garlic = gets.chomp
-  garlic.downcase!
+    puts "How old are you?"
+    age = gets.chomp.to_i
 
-  if garlic == ("yes" || "y" || "absolutely" || "sure")
-    garlic = true
-  elsif garlic == ("no" || "n" || "nope" || "noway")
-    garlic = false
-  end
+    puts "What year were you born?"
+    born = gets.chomp.to_i
 
-  puts "Would your like to enroll in the company's health insurance plan?"
-  insurance = gets.chomp
-  insurance.downcase!
+    puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+    garlic = gets.chomp
+    garlic.downcase!
 
-  if insurance == ("yes" || "y" || "absolutely" || "sure")
-    insurance = true
-  elsif insurance == ("no" || "n" || "nope" || "noway")
-    insurance = false
-  end
+    if garlic == ("yes" || "y" || "absolutely" || "sure")
+      garlic = true
+    elsif garlic == ("no" || "n" || "nope" || "noway")
+      garlic = false
+    end
 
-  current_year = Time.now.year
+    puts "Would your like to enroll in the company's health insurance plan?"
+    insurance = gets.chomp
+    insurance.downcase!
 
-  if name == "drake cula" || name == "tu fang"
-    result = "Definitely a vampire"
-  elsif (age == (current_year - born)) && (garlic || insurance)
-    result = "Probably not a vampire."
-  elsif (age = (current_year - born)) && (garlic && insurance)
-    result = "Almost certainly a vampire."
-  elsif (age = (current_year - born)) && (garlic || insurance )
-    result = "Probably a vampire."
-  else
-    result = "Results inconclusive"
-  end
+    if insurance == ("yes" || "y" || "absolutely" || "sure")
+      insurance = true
+    elsif insurance == ("no" || "n" || "nope" || "noway")
+      insurance = false
+    end
 
-puts result
-    
+    current_year = Time.now.year
+
+    allergies = [ ]
+    allergy_test = false
+    puts "Please list any allergies you have:"
+
+    while allergy_test == false
+      input = gets.chomp.downcase
+      allergies.push input
+
+      if input == "done"
+        allergy_test = true
+        if name == "drake cula" || name == "tu fang"
+          result = "Definitely a vampire"
+        elsif (age == (current_year - born)) && (garlic == true || insurance == true )
+          result = "Probably not a vampire."
+        elsif (age = (current_year - born)) && (garlic == false && insurance == false)
+          result = "Almost certainly a vampire."
+        elsif (age = (current_year - born)) && (garlic == false || insurance == false)
+          result = "Probably a vampire."
+        else
+          result = "Results inconclusive"
+        end
+      elsif input == "sunshine"
+        result = "Probably a vampire."
+        allergy_test = true
+      else
+        allergy_test = false
+      end
+    end
+
+    puts result
+
+    employees -= 1
+
+  end      
 end
 
 vamp()
