1fe662da4554237dcfae37e48cc51f957caa81d5
Collin Rea <collinr01@gmail.com>
2016-08-13 09:20:08 -0500

    created pseudocode for alias maker method for Solo project 5.6 -Initial commit

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,18 @@
+# Alias Maker Module
+
+# Define a method that takes a real name
+  
+  # Create "aeiou" variable
+  # Create consonant variable
+
+  # Split name by " " to separate first and last name into an array
+  # Reverse the array order
+  # Join name back together into a string
+
+  # Iterate through string to find vowels and consonants
+    # if vowel change to next vowel
+    # if consonant change to next consonant
+  
+  # return new alias
+#end
+
