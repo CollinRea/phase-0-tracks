8293f40cb08a46cb5160cca7c96c431a2a1a4e69
Collin Rea <collinr01@gmail.com>
2016-08-13 11:14:57 -0500

    updated method to add while loop to iterate through name and change letter to next vowel or next consonant

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -21,8 +21,32 @@ def alias_maker(name)
   vowels = "aeiouy"
   consonant = "bcdfghjklmnpqrstvwxz"
 
-  name.split(" ").reverse.join(" ")
+  name = name.split(" ").reverse.join(" ").downcase
 
+  i = 0
+  j = 0
+  new_name = ""
+
+  while i < name.length
+    if name[i] == vowels[j] || name[i] == consonant[j]
+      if name[i] == vowels[j]
+        new_name[i] = vowels[j + 1]
+        i += 1
+        j = 0
+      else
+        new_name[i] = consonant[j + 1]
+        i += 1
+        j = 0
+      end
+    elsif name[i] == " "
+      new_name[i] = name[i]
+      i += 1
+    else
+      j += 1
+    end
+  end
+  
+  new_name.split.each {|word| word.capitalize!}.join(" ")
 end
 
 p alias_maker("Collin Rea")
