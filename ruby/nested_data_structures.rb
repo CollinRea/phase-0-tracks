b31fb027e3844300a841f69f5aaaafecfcee8401
Collin Rea <collinr01@gmail.com>
2016-08-12 21:51:43 -0500

    created nested data structure for assignment 5.5

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,43 @@
+# A Grocery Store!
+
+grocery = {
+  deli: {
+    "Sandwiches" => [
+      "BLT",
+      "Italian",
+      "Cuban"
+    ],
+    "Fresh Meats" => [
+      "Ham",
+      "Turkey",
+      "Roast Beef"
+    ]
+  },
+  produce: {
+    "Fruit" => [
+      "Apples",
+      "Oranges",
+      "Strawberries",
+      "Kiwi"
+    ],
+    "Vegetables" => [
+      "Tomato",
+      "Potato",
+      "Carrots"
+    ]
+  },
+  employees: {
+    "Karen" => {
+      days_missed: 10,
+      promotable: false
+    },
+    "David" => {
+      days_missed: 1,
+      promotable: true
+    },
+    "Jessica" => {
+      days_missed: 0,
+      promotable: true
+    }
+  }
+}
