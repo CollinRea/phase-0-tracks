e76fed05ca9394d178331951939582087e7c9e45
Collin Rea <collinr01@gmail.com>
2016-08-15 21:59:12 -0500

    added pseudocode for methods to create, add items, remove, updated and print list

---
+++ ruby/gps2_2.rb
@@ -0,0 +1,40 @@
+# Method to create a list
+# input: string of items separated by spaces (example: "carrots apples cereal pizza")
+# steps: 
+  # create an empty hash
+  # split input string with space
+  # assign new array to hash as keys
+  # set default quantity of values to 1
+  # print the list to the console [can you use one of your other methods here?]
+# output: Hash 
+
+# Method to add an item to a list
+# input: item name and optional quantity
+# steps: 
+  # take item name and quantity if given
+  # add item name and quantity to list
+  # print the updated list
+# output: updated hash
+
+# Method to remove an item from the list
+# input: item name to be removed
+# steps:
+  # check the list for given item name
+  # IF found, remove from list
+  # print updated list
+# output: updated hash
+
+# Method to update the quantity of an item
+# input: item name to be updated and new quantity
+# steps:
+  # check if the item name exists in the list
+  # IF found, update value with given quantity
+  # print updated list
+# output: updated hash
+
+# Method to print a list and make it look pretty
+# input: take the list to be printed
+# steps: 
+  # iterate through list and print value and corresponding amount
+# output: printing items in list 
+
