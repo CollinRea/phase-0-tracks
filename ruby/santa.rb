6fdc5db213e056fe473b0d8f19ccef80bac97b1c
Collin Rea <collinr01@gmail.com>
2016-08-18 14:41:43 -0500

    Added instance variables and initialized multiple Santas with arrays and a loop

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -9,11 +9,22 @@
 
   # initialize should print "Initializing Santa instance ..."
 
+# Release  1 - Add Attributes
+
+  # gender - passed in on initialization
+  # ethnicity - passed in on initialization
+  # reindeer_ranking - an array of reindeer from most preferred to least
+  # age - not passed in on initialization and defaults to 0
 
 class Santa
 
-  def initialize
+  def initialize(gender,ethnicity)
     puts "Initializing Santa instance ..."
+    @name = "Santa"
+    @gender = gender
+    @ethnicity = ethnicity
+    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+    @age = 0
   end
 
   def speak
@@ -23,10 +34,24 @@ class Santa
   def eat_milk_and_cookies(cookie)
     puts "That was a good #{cookie}!"
   end
+
+  def about
+    puts "Santa is a #{@ethnicity} #{@gender}."
+    puts "Santa is #{@age} years old."
+    puts "Favorite Reindeer in order:"
+    puts "#{@reindeer_ranking}"
+  end
+
 end
 
-kris = Santa.new
+santas = []
 
-kris.speak
+santa_genders = ["male", "male", "female", "bigender", "neutral", "n/a"]
+santa_ethnicities = ["Asian", "White", "Latino", "Mixed", "African-American", "n/a"]
 
-kris.eat_milk_and_cookies("sugar cookie")
+santa_genders.length.times do |i|
+  santas << Santa.new(santa_genders[i], santa_ethnicities[i])
+end
+
+puts "#{santas}"
+
