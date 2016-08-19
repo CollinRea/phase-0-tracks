6e6a6027d98d996a4f1e9ef72aaefde1ade852e0
Collin Rea <collinr01@gmail.com>
2016-08-19 07:14:04 -0500

    Changed Shout module to be mixin and added Student and Teacher class to test

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,14 +1,26 @@
 
 module Shout
-  def self.yell_angrily(words)
+  def yell_angrily(words)
     words + "!!!" + " :(" 
   end
 
-  def self.yelling_happily(words)
+  def yelling_happily(words)
     words + "!!!" + " :)"
   end
 end
 
+class Student
+  include Shout
+end
 
-p Shout.yell_angrily("I can't believe it")
-p Shout.yelling_happily("I am so exited")
+class Teacher
+  include Shout
+end
+
+chris = Student.new
+puts chris.yelling_happily("I love math")
+puts chris.yell_angrily("Someone took my pencil")
+
+jennifer = Teacher.new
+puts jennifer.yell_angrily("Everybody sit down")
+puts jennifer.yelling_happily("Welcome to the third grade class")
