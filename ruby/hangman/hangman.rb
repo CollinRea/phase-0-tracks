5b77503faf4084d9aeedb01d6ff17137785ac3b3
Collin Rea <collinr01@gmail.com>
2016-08-25 13:35:42 -0500

    Added game_over method to check if tries are maxed out and added a tries_max variable that can be changed by admin

--- ruby/hangman/hangman.rb
+++ ruby/hangman/hangman.rb
@@ -1,11 +1,13 @@
 
 class Hangman
   attr_reader :tries, :misses
+  attr_accessor :tries_max      #This will allow the admin to set max tries
   
   def initialize(correct_word)
     @correct_word = correct_word.downcase
     @guess_spaces = "_" * correct_word.length
     @tries = 0
+    @tries_max = 6
     @misses = []
     puts "Generating word... Good Luck!"
     
@@ -25,7 +27,7 @@ class Hangman
   
   def check_tries(guess_letter)
     if @tries >= 6
-      puts "I am sorry, you are out of tries! Game Over :("
+      game_over
       false
     elsif @correct_word.include? guess_letter
       puts "Correct!"
@@ -33,7 +35,9 @@ class Hangman
     else
       @tries += 1
       @misses << guess_letter
-      puts "Sorry #{guess_letter} is not a letter in this word."
+      puts "Sorry '#{guess_letter}' is not a letter in this word."
+      puts "You have #{@tries_max - @tries} tries left."
+      game_over
       false
     end
   end
@@ -42,10 +46,21 @@ class Hangman
     @guess_spaces.capitalize.split("").join(" ")
   end
   
+  def game_over
+    if @tries >= 6
+      puts "Sorry! You are out of tries... the correct word was:"
+      puts @correct_word.capitalize.split("").join(" ")
+    end
+  end
+
   def game_complete
-     puts "Game over! You Win!" if @correct_word == @guess_spaces
+    puts "Game over! You Win!" if @correct_word == @guess_spaces
   end
 end
+
+# USER INTERFACE
+
+
 
 # DRIVER CODE
 
@@ -61,4 +76,3 @@ first_game.guess("d")
 first_game.guess("h")
 p first_game.misses
 p first_game.tries
-first_game.guess("u")
