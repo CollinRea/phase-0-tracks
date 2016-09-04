17bd8c63688969c8316345f1c3292233786c98ae
Collin Rea <collinr01@gmail.com>
2016-09-04 09:28:01 -0500

    added daycare_review function that takes the rating, recoomended and daycare ID, and returns the ID. Next is user_interface to add a menu for the user and call the different functions. Also a display_review function

--- ruby/8.5/daycare.rb
+++ ruby/8.5/daycare.rb
@@ -34,12 +34,21 @@ SQL_TABLE
   $DB.execute(create_reviews_cmd)
 
 
+# function to add daycare which takes 5 arguments and passes to SQLite
 def add_daycare(name, loc, min_age, max_age, spots)
   new_daycare = "INSERT INTO daycares (name, loc, min_age, max_age, spots_avail) VALUES (?, ?, ?, ?, ?)"
   $DB.execute(new_daycare, [name, loc, min_age, max_age, spots])
 end
 
 
+# function to add daycare review and return id
+def daycare_review(rating, reco, id)
+  $DB.execute("INSERT INTO reviews (rating, recommended, daycare_id) VALUES (?, ?, ?)", [rating, reco, id])
+  id
+end
+
+
+# function to retrieve daycares from SQLite and display to user
 def display_daycares
   added_daycares = $DB.execute("SELECT * FROM daycares")
   
@@ -58,12 +67,27 @@ def display_daycares
 end
 
 
+
+
+
+
+
 # Driver Test Code
 
 # add_daycare("KinderCare", "Schaumburg", 0, 7, "true")
 # add_daycare("Jennifer's Kids", "Schaumburg", 0, 6, "true")
 
 display_daycares
+
+# daycare_review(4,'true',2)
+
+# $DB.execute("SELECT name, loc, rating, recommended FROM reviews JOIN daycares ON daycares.id = daycare_id")
+
+# puts "Review for daycare #{daycare_review(2,'false', 2)} added."
+
+
+
+
 
 
 
