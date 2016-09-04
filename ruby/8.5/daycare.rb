e411ac223e1866fb082037f53f11c5935bd83029
Collin Rea <collinr01@gmail.com>
2016-09-04 08:16:14 -0500

    added display_daycares function

--- ruby/8.5/daycare.rb
+++ ruby/8.5/daycare.rb
@@ -17,7 +17,7 @@ create_table_cmd = <<-SQL_TABLE
     max_age INT,
     spots_avail BOOLEAN
   )
-  SQL_TABLE
+SQL_TABLE
 
 # create table in db for reviews
 create_reviews_cmd = <<-SQL_TABLE 
@@ -28,14 +28,43 @@ create_reviews_cmd = <<-SQL_TABLE
     daycare_id INT,
     FOREIGN KEY (daycare_id) REFERENCES daycares(id)
   )
-  SQL_TABLE
+SQL_TABLE
 
   $DB.execute(create_table_cmd)
   $DB.execute(create_reviews_cmd)
+
 
 def add_daycare(name, loc, min_age, max_age, spots)
   new_daycare = "INSERT INTO daycares (name, loc, min_age, max_age, spots_avail) VALUES (?, ?, ?, ?, ?)"
   $DB.execute(new_daycare, [name, loc, min_age, max_age, spots])
 end
 
+
+def display_daycares
+  added_daycares = $DB.execute("SELECT * FROM daycares")
+  
+  added_daycares.each_with_index do |daycare, index|
+    puts "-- Daycare #{index + 1} --"
+    puts "#{daycare['name']} in #{daycare['loc']}"
+    if daycare['min_age'] == 0
+      puts "Youngest age accepted: Newborns"
+    else
+      puts "Youngest age accepted: #{daycare['min_age']}"
+    end
+    puts "Oldest age accepted: #{daycare['max_age']}"
+    puts "Spots available: #{daycare['spots_avail']}"
+    puts "---------------"
+  end
+end
+
+
+# Driver Test Code
+
 # add_daycare("KinderCare", "Schaumburg", 0, 7, "true")
+# add_daycare("Jennifer's Kids", "Schaumburg", 0, 6, "true")
+
+display_daycares
+
+
+
+
